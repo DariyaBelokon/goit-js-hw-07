20760138e6ebb15596efa6ad3474916792c57a1e
DariyaBelokon <vors.zp@gmail.com>
2021-06-21 14:09:09 +0300

    Выполнены все 8 заданий

--- js/task-08.js
+++ js/task-08.js
@@ -6,14 +6,20 @@ const buttonCreate = buttonAll[0];
 const buttonRemove = buttonAll[1];
 const box = document.getElementById('boxes');
 
-
-const colors = ["red", "blue", "green"];
-
+ const getRandom = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 const createBoxes = amount => {
     amount = input.value;
     const collection = [];
     for (let i = 0; i < amount; i++) {
-        collection.push(document.createElement('div'));
+        const newElement = document.createElement('div');
+        collection.push(newElement);
+        const randomWidth = 30 + 10 * i;
+        const randomHeight = 30 + 10 * i;
+        newElement.style.width = `${randomWidth}px`;
+        newElement.style.height = `${randomHeight}px`;
+        newElement.style.backgroundColor = `rgb(${getRandom(0, 255)},${getRandom(0, 255)},${getRandom(0, 255)})`;
     }
     box.append(...collection);
     console.log(box);
@@ -21,15 +27,10 @@ const createBoxes = amount => {
 buttonCreate.addEventListener('click', createBoxes);
 buttonRemove.addEventListener('click', e => {
     let children = box.childNodes;
-    // НЕ СРАБОТАЛО! не удалило элементы!
-    for (let i = 0; i < children; i++) {
-        box.remove(children[i]);
-    }
-    // Удалило только первый элемент, найти вариант!
-    //  if (box.firstChild) {
-    //      box.firstChild.remove();
-    //     console.log(box);
-    //  }
     
+    if (box.firstChild) {
+        box.innerHTML = '';
+        console.log(box);
+    }
 }
-)
+);
